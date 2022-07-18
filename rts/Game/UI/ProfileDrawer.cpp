2ec291757c1bf5c6910548b3dfd68d484826aea2
LHoG <1476261+lhog@users.noreply.github.com>
2022-07-19 01:53:07 +0400

    Experimental change on projectiles containers

--- rts/Game/UI/ProfileDrawer.cpp
+++ rts/Game/UI/ProfileDrawer.cpp
@@ -493,7 +493,7 @@ static void DrawInfoText(TypedRenderBuffer<VA_TYPE_C   >& rb)
 	);
 
 	font->glFormat(0.01f, 0.08f, 0.5f, DBG_FONT_FLAGS, spdFmtStr, gs->speedFactor, gs->wantedSpeedFactor);
-	font->glFormat(0.01f, 0.10f, 0.5f, DBG_FONT_FLAGS, sfxFmtStr, ph->projectileContainers[true].size(), ph->projectileContainers[false].size(), ph->GetCurrentParticles(), ph->GetParticleSaturation(true));
+	font->glFormat(0.01f, 0.10f, 0.5f, DBG_FONT_FLAGS, sfxFmtStr, ph->GetActiveProjectiles(true).size(), ph->GetActiveProjectiles(false).size(), ph->GetCurrentParticles(), ph->GetParticleSaturation(true));
 
 	{
 		const int2 pfsUpdates = pm->GetNumQueuedUpdates();
